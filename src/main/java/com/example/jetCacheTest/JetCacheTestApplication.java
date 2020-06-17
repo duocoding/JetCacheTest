5f92f033c8554cdfefefbcdf71e9a50d8b56a6fe
duocoding <48700579+duocoding@users.noreply.github.com>
2020-06-17 13:01:02 +0800

    creat repository

---
+++ src/main/java/com/example/jetCacheTest/JetCacheTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jetCacheTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetCacheTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetCacheTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jetCacheTest/JetCacheTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jetCacheTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetCacheTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
